4f38e2fd7772312596a9e9862336bbf9e3eebec1
Eric R <ryttingeric@gmail.com>
2022-11-28 15:31:22 -0700

    removed unneeded variable. split get out of jail stratiges into 2 private methods called by the player turn method. added getter for the landed on squares. fixed bug causing player position to go beyond 40.

--- src/Monopoly/Player.java
+++ src/Monopoly/Player.java
@@ -14,9 +14,7 @@ public class Player {
     /**
      * represents the number of times a boardsquare has been landed on.
      */
-    private int landedOnSquares;
-
-    private boolean inJail;
+    private int[] landedOnSquares;
     
     /**
      * the current square the player is on.
@@ -27,24 +25,28 @@ public class Player {
      * number of turns in jail
      */
     private int turnsInJail;
+    private boolean useCardImmediately;
     
     /**
      * 
      * @param monopoly
      */
-    public Player(Monopoly monopoly) {
+    public Player(Monopoly monopoly, boolean useCardImmediately) {
     	this.monopoly = monopoly;
+        this.useCardImmediately = useCardImmediately;
+        landedOnSquares = new int[monopoly.getGameBoard().length];
     	//Space 0 will be designated as the GO square
     	setCurrentSquare(0);
     }
 
     // Getter And Setter Methods
-    public void setInJail(boolean inJail) {
-        this.inJail = inJail;
-    }
 
     public int getOutOfJailFreeCards() {
         return outOfJailFreeCards;
+    }
+
+    public int[] getLandedOnSquares() {
+        return landedOnSquares;
     }
 
     public Monopoly getMonopoly() {
@@ -71,14 +73,13 @@ public class Player {
     	//roll the dice
     	int doubles = 0;
     	int rollsLeft = 1;
-    	Die die = new Die();
     	int roll1;
     	int roll2;
     	
     	//Put as a loop for ease of handling die roll
     	while(doubles < 2 || rollsLeft > 0) {
-    		roll1 = die.Roll();
-    		roll2 = die.Roll();
+    		roll1 = Die.Roll();
+    		roll2 = Die.Roll();
     		rollsLeft--;
     		
 	    	//check for doubles
@@ -88,63 +89,47 @@ public class Player {
 	    	}
 	
 	    	//if not on jail, go amount on dice and set current square
-	    	if(currentSquare != 40) {
-	    		// Reason why I didn't use the setter is because there are issues with going over amount of board spaces
-	    		currentSquare = currentSquare + roll1 + roll2 % 39;
-	    		
-	    		doAction();
+	    	if(currentSquare == 40) {
+                if (useCardImmediately){
+                    useCardImmediately(doubles);
+                } else {
+                    useCardLater(doubles);
+                }
+
 	    	} else {
-	    		if(getOutOfJailFreeCards() > 0) {
-	    			currentSquare = 11;
-	    			outOfJailFreeCards--;
-	    		} else if(doubles > 0){
-	    			currentSquare = 11;
-	    		}
+                currentSquare = (currentSquare + roll1 + roll2) % 39;
+
+                doAction();
 	    	}
+            if (doubles == 4){
+                currentSquare = 40;
+            }
+            landedOnSquares[currentSquare]++;
     	}
 
     }
-    
-    /**
-     * Method for doing the get out of jail on the 4th turn.
-     */
-    // I am really tired right now, and can't think up a better way to do this.
-    public void DoAlternateTurn() {
-    	int doubles = 0;
-    	int rollsLeft = 1;
-    	Die die = new Die();
-    	int roll1;
-    	int roll2;
-    	
-    	while(doubles < 2 || rollsLeft > 0) {
-    		roll1 = die.Roll();
-    		roll2 = die.Roll();
-    		rollsLeft--;
-    		
-	    	if(roll1 == roll2) {
-	    		doubles++;
-	    		rollsLeft++;
-	    	}
-	
-	    	//if not on jail, go amount on dice and set current square
-	    	if(currentSquare != 40) {
-	    		currentSquare = currentSquare + roll1 + roll2 % 39;
-	    		
-	    		doAction();
-	    	} else {
-	    		turnsInJail++;
-	    		
-	    		if(getOutOfJailFreeCards() > 0 && turnsInJail == 4) {
-	    			currentSquare = 11;
-	    			outOfJailFreeCards--;
-	    			turnsInJail = 0;
-	    		} else if(doubles > 0){
-	    			currentSquare = 11;
-	    			turnsInJail = 0;
-	    		}
-	    	}
-    	}
-    	
+
+    private void useCardImmediately(int doubles) {
+        if(getOutOfJailFreeCards() > 0) {
+            //Should this be 10? was 11.
+            currentSquare = 10;
+            outOfJailFreeCards--;
+        } else if(doubles > 0){
+            currentSquare = 10;
+        }
+    }
+
+    private void useCardLater(int doubles) {
+        turnsInJail++;
+
+        if(getOutOfJailFreeCards() > 0 && turnsInJail == 4) {
+            currentSquare = 11;
+            outOfJailFreeCards--;
+            turnsInJail = 0;
+        } else if(doubles > 0){
+            currentSquare = 11;
+            turnsInJail = 0;
+        }
     }
     
     private void doAction() {
