6639908fb149605031720799181a590411dbe58d
Eric R <ryttingeric@gmail.com>
2022-11-28 15:25:10 -0700

    added 2 more card classes for needed functionality

---
+++ src/Monopoly/Cards/MoveToRail.java
@@ -0,0 +1,21 @@
+package Monopoly.Cards;
+
+import Monopoly.Player;
+
+public class MoveToRail extends Card{
+    public MoveToRail(String name) {
+        super(name);
+    }
+
+    @Override
+    public void Action(Player player) {
+        int currentSquare = player.getCurrentSquare();
+        int[] railStations = {5, 15, 25, 35};
+
+        for (int j : railStations) {
+            if (currentSquare < j) {
+                player.setCurrentSquare(j);
+            }
+        }
+    }
+}
---
+++ src/Monopoly/Cards/MoveToUtility.java
@@ -0,0 +1,21 @@
+package Monopoly.Cards;
+
+import Monopoly.Player;
+
+public class MoveToUtility extends Card{
+    public MoveToUtility(String name) {
+        super(name);
+    }
+
+    @Override
+    public void Action(Player player) {
+        int currentSquare = player.getCurrentSquare();
+        int[] utilities = {12, 28};
+
+        for (int j : utilities) {
+            if (currentSquare < j) {
+                player.setCurrentSquare(j);
+            }
+        }
+    }
+}
